a87913f5702cc715dcfb9c358d2ee4e6515875f1
george697 <115705682+george697@users.noreply.github.com>
2025-02-13 18:15:14 +0000

    Login API specification added to README file.

--- onboarding/urls.py
+++ onboarding/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path("create-user/", views.CreateAccountView().as_view())
+    path("create-user/", views.CreateAccountView.as_view(), name="create-user"),
+    path("login-user/", views.LoginView.as_view(), name="login-user"),  # 🔹
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
 ]
--- onboarding/views.py
+++ onboarding/views.py
@@ -2,11 +2,12 @@ from rest_framework import generics, permissions
 from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from rest_framework_simplejwt.tokens import RefreshToken
 from utils.api import api_error, api_success, check_input
 from utils.strings import Messages
 from .user_serializers import CreateUserSerializer
 from django.core import serializers
+from django.contrib.auth import authenticate
 import json
 
 
@@ -35,3 +36,31 @@ class CreateAccountView(generics.CreateAPIView):
             })
         except KeyError as keyErr:
             return api_error('{} is missing'.format(keyErr.__str__()))
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+    return {
+        'refresh': str(refresh),  # Refresh token (Used to get a new access token)
+        'access': str(refresh.access_token),  # Main token used for authentication
+    }
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if not username or not password:
+            return api_error("Username and password are required")
+
+        user = authenticate(username=username, password=password)
+        if user:
+            tokens = get_tokens_for_user(user)  # Generate JWT tokens
+            return api_success({
+                "username": user.username,
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "email": user.email,
+                "token": tokens['access'],  # Return access token for authentication
+                "refresh_token": tokens['refresh'],  # Refresh token for re-authentication
+            })
+        return api_error("Invalid username or password")
