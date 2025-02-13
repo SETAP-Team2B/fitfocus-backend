4be29cfa495f0a14e7eeee31844fbd6711e18e02
george697 <115705682+george697@users.noreply.github.com>
2025-02-13 18:19:55 +0000

    Login API specification added to README file.

--- onboarding/urls.py
+++ onboarding/urls.py
@@ -4,6 +4,6 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("create-user/", views.CreateAccountView.as_view(), name="create-user"),
-    path("login-user/", views.LoginView.as_view(), name="login-user"),  # 🔹
+    path("login-user/", views.LoginView.as_view(), name="login-user"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
 ]
