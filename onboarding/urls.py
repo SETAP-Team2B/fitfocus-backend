0e218754070ba63869e1b5f6154721ad709026ea
Bradley Smith <smithbradley913@gmail.com>
2025-02-14 01:20:50 +0000

    added OTP urls

--- onboarding/urls.py
+++ onboarding/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("create-user/", views.CreateAccountView().as_view())
+    path("create-user/", views.CreateAccountView().as_view()),
+    path("generate-otp/", views.CreateGetOTPView().as_view()),
+    path("validate-otp/", views.ValidateOTPView().as_view())
 ]
