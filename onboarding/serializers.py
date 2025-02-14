ef7348732d7ef4d0d554071a925f66cfd03912dd
Bradley Smith <smithbradley913@gmail.com>
2025-02-14 01:20:31 +0000

    renamed user_serializers.py to serializers.py, added OTP serializer

--- onboarding/serializers.py
+++ onboarding/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-
+from .models import OTP
 
 class CreateUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = '__all__'
+
+class CreateOTPSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OTP
         fields = '__all__'
