0b71aee10ad518c17aef914048f1f378a50cbc66
Bradley Smith <smithbradley913@gmail.com>
2025-02-26 17:12:35 +0000

    created loggedexercise model and serializer

--- onboarding/serializers.py
+++ onboarding/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import OTP
-from .models import Exercise
+from .models import OTP, Exercise, LoggedExercise
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,4 +15,9 @@ class OTPSerializer(serializers.ModelSerializer):
 class ExerciseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Exercise
+        fields = '__all__'
+
+class LoggedExerciseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LoggedExercise
         fields = '__all__'
