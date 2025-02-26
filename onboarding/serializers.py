bd28e36b54e264202f9f1bb17f2ca1cc4a2f4ead
Bradley Smith <smithbradley913@gmail.com>
2025-02-26 17:04:05 +0000

    renamed serializers for understanding

--- onboarding/serializers.py
+++ onboarding/serializers.py
@@ -3,28 +3,17 @@ from django.contrib.auth.models import User
 from .models import OTP
 from .models import Exercise
 
-class CreateUserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
 
-class CreateOTPSerializer(serializers.ModelSerializer):
+class OTPSerializer(serializers.ModelSerializer):
     class Meta:
         model = OTP
         fields = '__all__'
 
-class CreateExerciseSerializer(serializers.ModelSerializer):
+class ExerciseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Exercise
         fields = '__all__'
-
-class GetExerciseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Exercise
-        fields = (
-            'ex_name',
-            'ex_type',
-            'ex_body_area',
-            'equipment_needed',
-            'ex_target_muscle'
-        )
